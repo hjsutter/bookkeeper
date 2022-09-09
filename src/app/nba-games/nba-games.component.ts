ffba3cb54a823547e8b6eff223a927348969b1ba
hjsutter <hjsutter@gmail.com>
2022-09-09 00:42:20 -0500

    Updated NBA page to match MLB

--- src/app/nba-games/nba-games.component.ts
+++ src/app/nba-games/nba-games.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import axios from 'axios';
 
 let apiKey = '2a24d9b96c09d00f9b220a306b5d9f7a'
 let sportKey = 'basketball_nba'
 let regions ='us'
 let markets = 'h2h'
-let oddsFormat = 'decimal'
-let dateFormat = 'iso'
 let bookmakers = ['barstool', 'betus','betmgm', 'circasports', 'draftkings', 'fanduel', 'foxbet', 'mybookieag', 'pointsbetus', 'unibet']
 
 @Component({
@@ -17,28 +16,34 @@ let bookmakers = ['barstool', 'betus','betmgm', 'circasports', 'draftkings', 'fa
 export class NbaGamesComponent implements OnInit {
 
   oddsList: any[] = [];
+  betLines: any = {};
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    //Get the odds for each upcoming NBA game
-    // axios.get(`https://api.the-odds-api.com/v4/sports/${ sportKey }/odds/?apiKey=${ apiKey }&regions=${ regions }&markets=${ markets }&bookmakers=${ bookmakers }`)
-    // .then(response => {
-    //   console.log(response.data)
-    //   this.oddsList = response.data
-    //   console.log('Remaining requests', response.headers['x-requests-remaining'])
-    //   console.log('Used requests', response.headers['x-requests-used'])
-    // })
-    // .catch(error => {
-    //   console.log('Error stats', error.response.data)
-    //   console.log(error.response.data)
-    // })
+   //Get the odds for each upcoming NBA game
+   axios.get(`https://api.the-odds-api.com/v4/sports/${ sportKey }/odds/?apiKey=${ apiKey }&regions=${ regions }&markets=${ markets }&bookmakers=${ bookmakers }`)
+   .then(response => {
+     console.log(response.data)
+     this.oddsList = response.data
+     this.oddsList.sort()
+     let formattedDate = new Date();
+     for(let i = 0; i < this.oddsList.length; i++){
+       formattedDate = new Date(this.oddsList[i].commence_time);
+       this.oddsList[i].commence_time = formattedDate.toDateString()
+     }
+     console.log('Remaining requests', response.headers['x-requests-remaining'])
+     console.log('Used requests', response.headers['x-requests-used'])
+   })
+   .catch(error => {
+     console.log('Error stats', error.response.data)
+     console.log(error.response.data)
+   })
   }
 
-  viewDetails(index: any): void {
-    console.log(index)
-    console.log(this.oddsList[index])
-
+  viewOdds(i: number): void {
+    this.betLines = [this.oddsList[i]];
+    this.router.navigate(['/odds'], {state: {data: this.betLines}})
   }
 
 }
