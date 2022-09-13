d8ed610ce46d9f508b9d27117ea9a0dce7fdea34
Hunter Sutter <hjsutter@gmail.com>
2022-09-12 23:43:14 -0500

    Deleted individual sport components. SEE: odds-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NbaGamesComponent } from './nba-games/nba-games.component';
-import { MlbGamesComponent } from './mlb-games/mlb-games.component';
-import { NflGamesComponent } from './nfl-games/nfl-games.component';
-import { NhlGamesComponent } from './nhl-games/nhl-games.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { OddsComponent } from './odds/odds.component';
 import { OddsListComponent } from './odds-list/odds-list.component';
 
 const routes: Routes = [
   {path: 'home', component: HomepageComponent},
-  {path: 'mlb', component: MlbGamesComponent},
-  {path: 'nba', component: NbaGamesComponent},
-  {path: 'nfl', component: NflGamesComponent},
-  {path: 'nhl', component: NhlGamesComponent},
   {path: 'odds', component: OddsComponent},
   {path: 'odds-list', component: OddsListComponent},
   {path: '**', component: HomepageComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MlbGamesComponent } from './mlb-games/mlb-games.component';
-import { NbaGamesComponent } from './nba-games/nba-games.component';
-import { NflGamesComponent } from './nfl-games/nfl-games.component';
-import { NhlGamesComponent } from './nhl-games/nhl-games.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { OddsComponent } from './odds/odds.component';
 import { OddsListComponent } from './odds-list/odds-list.component';
@@ -14,10 +10,6 @@ import { OddsListComponent } from './odds-list/odds-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MlbGamesComponent,
-    NbaGamesComponent,
-    NflGamesComponent,
-    NhlGamesComponent,
     HomepageComponent,
     OddsComponent,
     OddsListComponent
